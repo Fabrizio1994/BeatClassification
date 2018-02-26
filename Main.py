e66e6cbb62b0870945e7f4b03afc9a2467438c96
alescire94 <alescire94@gmail.com>
2018-02-26 18:32:57 +0100

    Knn exectution and grid search clean from non beat in Utility class

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
 import wfdb
 import numpy as np
 from FeatureExtraction import FeatureExtraction
+from KNN import KNN
+from GridSearch import GridSearch
 
 fe = FeatureExtraction()
-NON_BEAT_ANN = [ '[', '!', ']', 'x', '(', ')', 'p', 't', 'u', '`', '\'', '^', '|', '~', '+', 's', 'T', '*', 'D', '='
-    ,'"', '@']
-fe.extract_features('sample/100')
 
 
+train_features, train_labels = fe.extract_features('sample/100')
+test_features, test_labels = fe.extract_features('sample/215')
+knn = KNN(train_features, train_labels, test_features, test_labels)
+#GridSearch(train_features, train_labels, test_features, test_labels, ["not QRS", "QRS"])
 
 
--- Utility.py
+++ Utility.py
@@ -1,9 +1,14 @@
 import wfdb
+import numpy as np
 
 class Utility:
+
+    NON_BEAT_ANN = [ '[', '!', ']', 'x', '(', ')', 'p', 't', 'u', '`', '\'', '^', '|', '~', '+', 's', 'T', '*', 'D', '='
+        ,'"', '@']
+
     # CODE TO CLEAN SIGNAL FROM NON BEAT ANNOTATIONS
-    def remove_non_beat(self):
-        annotation = wfdb.rdann("sample/100","atr")
+    def remove_non_beat(self, sample_name):
+        annotation = wfdb.rdann(sample_name,"atr")
         new_sample = []
         new_symbol= []
         samples = annotation.sample
@@ -11,10 +16,10 @@ class Utility:
         j = 0
 
         for j in range(len(samples)):
-            if(symbols[j] not in NON_BEAT_ANN):
+            if symbols[j] not in self.NON_BEAT_ANN:
                 new_sample.append(samples[j])
                 new_symbol.append(symbols[j])
 
         new_sample = np.asarray(new_sample)
         new_symbol = np.asarray(new_symbol)
-        wfdb.wrann("100","atr", new_sample, new_symbol)
+        wfdb.wrann(sample_name.replace("sample/", ""),"atr", new_sample, new_symbol)
